796642ed0804b36b731aeb074a2a87585ca7922b
sxfcx <valtteri.kunttu@gmail.com>
2025-01-28 08:28:40 +0200

    Update PTHT BY SXFCC.lua

--- PTHT BY SXFCC.lua
+++ PTHT BY SXFCC.lua
@@ -3,6 +3,6 @@ return {
 487237, -- MrayLiegh
 902719,-- DarkSctyhe, discord user ID 1191799069165756478
 833580, -- Rapz 2hs discord user ID 1162163740398059560
-180854 -- skylark user id 758656384161415178
+180854, -- skylark user id 758656384161415178
 139256 -- RAYJET TTT
 } 
